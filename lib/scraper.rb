268fbb0f61ecbd9a68a54a36b5afd899fd0dff3c
Gabriel Taylor-Russ <gabriel.taylor.russ@gmail.com>
2015-03-09 21:37:47 -0700

    Increase request timeout to 30 seconds. Bumped version

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,14 +7,14 @@ module Scraper
   # https://www.hipmunk.com/flights#f=YVR;t=NYC;d=2015-03-06;r=2015-03-08
   @base = "https://www.hipmunk.com/flights#"
 
-  def self.request(from_code, to_code, departing, returning, base_url=nil)
+  def self.request(from_code:, to_code:, departing:, returning:, base_url: nil)
     base_url = base_url || @base
     url = "#{base_url}f=#{from_code};t=#{to_code};d=#{departing};r=#{returning}"
 
     headless = Headless.new
     headless.start
     browser = Watir::Browser.start url
-    browser.div(:class => "flight-tab-group-info__price-bottom").wait_until_present(12)
+    browser.div(:class => "flight-tab-group-info__price-bottom").wait_until_present(30)
     text = browser.div(:class => "flight-tab-group-info__price-bottom").text
     headless.destroy
 
